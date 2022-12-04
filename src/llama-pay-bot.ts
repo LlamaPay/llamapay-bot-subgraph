63fc884f34adcecc07cfadaa0a3ffd014535d166
nemusona <nemusonaneko@protonmail.com>
2022-12-04 01:20:54 -0600

    init

--- src/llama-pay-bot.ts
+++ src/llama-pay-bot.ts
@@ -1,64 +1,48 @@
-import { BigInt } from "@graphprotocol/graph-ts"
 import {
-  LlamaPayBot,
   WithdrawCancelled,
   WithdrawExecuted,
-  WithdrawScheduled
-} from "../generated/LlamaPayBot/LlamaPayBot"
-import { ExampleEntity } from "../generated/schema"
+  WithdrawScheduled,
+} from "../generated/LlamaPayBot/LlamaPayBot";
+import { Schedule, Token } from "../generated/schema";
+import { ERC20 } from "../generated/LlamaPayBot/ERC20";
 
-export function handleWithdrawCancelled(event: WithdrawCancelled): void {
-  // Entities can be loaded from the store using a string ID; this ID
-  // needs to be unique across all entities of the same type
-  let entity = ExampleEntity.load(event.transaction.from)
-
-  // Entities only exist after they have been saved to the store;
-  // `null` checks allow to create entities on demand
-  if (!entity) {
-    entity = new ExampleEntity(event.transaction.from)
-
-    // Entity fields can be set using simple assignments
-    entity.count = BigInt.fromI32(0)
+export function handleWithdrawScheduled(event: WithdrawScheduled): void {
+  let entity = Schedule.load(event.params.id.toHexString());
+  if (entity === null) {
+    entity = new Schedule(event.params.id.toHexString());
+    let token = Token.load(event.params.token.toHexString());
+    if (token === null) {
+      token = new Token(event.params.token.toHexString());
+      const erc20 = ERC20.bind(event.params.token);
+      token.address = event.params.token;
+      token.symbol = erc20.try_symbol().value;
+      token.name = erc20.try_name().value;
+      token.decimals = erc20.try_decimals().value;
+      token.save();
+    }
+    entity.scheduleId = event.params.id;
+    entity.owner = event.params.owner;
+    entity.token = token.id;
+    entity.from = event.params.from;
+    entity.to = event.params.to;
+    entity.amountPerSec = event.params.amountPerSec;
+    entity.starts = event.params.starts;
+    entity.nextUpdate = event.params.starts;
   }
-
-  // BigInt and BigDecimal math are supported
-  entity.count = entity.count + BigInt.fromI32(1)
-
-  // Entity fields can be set based on event parameters
-  entity.owner = event.params.owner
-  entity.token = event.params.token
-
-  // Entities can be written to the store with `.save()`
-  entity.save()
-
-  // Note: If a handler doesn't require existing field values, it is faster
-  // _not_ to load the entity from the store. Instead, create it fresh with
-  // `new Entity(...)`, set the fields that should be updated and save the
-  // entity back to the store. Fields that were not set or unset remain
-  // unchanged, allowing for partial updates to be applied.
-
-  // It is also possible to access smart contracts from mappings. For
-  // example, the contract that has emitted the event can be connected to
-  // with:
-  //
-  // let contract = Contract.bind(event.address)
-  //
-  // The following functions can then be called on this contract to access
-  // state variables and other data:
-  //
-  // - contract.balances(...)
-  // - contract.bot(...)
-  // - contract.calcWithdrawId(...)
-  // - contract.cancelWithdraw(...)
-  // - contract.factory(...)
-  // - contract.fee(...)
-  // - contract.llama(...)
-  // - contract.newLlama(...)
-  // - contract.owners(...)
-  // - contract.redirects(...)
-  // - contract.scheduleWithdraw(...)
+  entity.active = true;
+  entity.save();
 }
 
-export function handleWithdrawExecuted(event: WithdrawExecuted): void {}
+export function handleWithdrawExecuted(event: WithdrawExecuted): void {
+  let entity = Schedule.load(event.params.id.toHexString());
+  if (!entity) return;
+  entity.nextUpdate = event.block.timestamp.plus(entity.frequency);
+  entity.save();
+}
 
-export function handleWithdrawScheduled(event: WithdrawScheduled): void {}
+export function handleWithdrawCancelled(event: WithdrawCancelled): void {
+  let entity = Schedule.load(event.params.id.toHexString());
+  if (!entity) return;
+  entity.active = false;
+  entity.save();
+}
